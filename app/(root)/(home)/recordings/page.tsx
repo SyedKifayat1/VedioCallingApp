c8b5c94ceab8a5ffc52063de49d57dbb2d615fed
Syed Kifayat <bsce22026@itu.edu.pk>
2024-07-26 03:50:28 +0500

    commit

---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CallList from '@/components/ui/CallList'
+
+const Recordings = () => {
+  return (
+    <section className='flex size-full flex-col gap-10 text-white'>
+        <h1 className="text-3xl font-bold">
+            Recording
+        </h1>
+        <CallList type='recordings'/>
+    </section>
+  )
+}
+
+export default Recordings
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react'
+export const metadata: Metadata = {
+    title: "YOOM",
+    description: "Vedio Calling App",
+  };
+const RootLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <main>
+            <StreamVideoProvider>
+            {children}
+            </StreamVideoProvider>
+        </main>
+    )
+}
+
+export default RootLayout
